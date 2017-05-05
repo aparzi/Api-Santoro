434cbf682cdbffc051e433f87238443f42da57e7
Angelo Parziale <angeloparziale94@gmail.com>
2017-05-05 21:00:25 +0200

    Update home.php

--- routes/home.php
+++ routes/home.php
@@ -3,7 +3,7 @@ $app->get('/', function($request, $response, $args) {
     $string = '<h2>Select your route:</h2>
               <ul>
               <li><strong>/news</strong> = shows all news</li>
-              <li><strong>/orari</strong> = shows all times</li>
+              <li><strong>/orari/{paesePartenza}/{idDestinazione}</strong> = shows all times</li>
               </ul>
               ';
     $response->write($string);
