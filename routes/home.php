4462dcbda57cf9cc9cb93e0aa14f074ca4e01f05
Angelo <angeloparziale94@gmail.com>
2017-05-11 12:47:36 +0200

    new updates

--- routes/home.php
+++ routes/home.php
@@ -3,7 +3,10 @@ $app->get('/', function($request, $response, $args) {
     $string = '<h2>Select your route:</h2>
               <ul>
               <li><strong>/news</strong> = shows all news</li>
-              <li><strong>/orari/{paesePartenza}/{idDestinazione}</strong> = shows all times</li>
+              <br />
+              <li><strong>/orari/{paesePartenza}/{idDestinazione}</strong> = shows all bus times</li>
+              <br />
+              <li><strong>/fermate/{id_paese}</strong> = shows all bus stop</li>
               </ul>
               ';
     $response->write($string);
